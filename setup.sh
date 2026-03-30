c904e68b097aa9706f1ff1f49e997ae4b23f6dad
Aurèle Habbard <aurelehabbard@gmail.com>
2026-03-30 15:58:41 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#! bin/bash
+#!/bin/bash
 python3 -m venv venv_nlp
 source venv_nlp/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
